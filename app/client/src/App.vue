76da818a60d2e511480971ea2ac6952f47f6dcb7
dmatijani <dmatijani21@foi.hr>
2025-01-02 11:57:05 +0100

    Dodan prazni vue project

---
+++ app/client/src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import Card from './components/Card.vue'
+</script>
+
+<template>
+  <header>
+    <h1>TUD</h1>
+  </header>
+
+  <main>
+    <p>Ja radim!</p>
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+  </main>
+</template>
+
+<style scoped>
+
+</style>
