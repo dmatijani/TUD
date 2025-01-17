87b6fb6d8246cfb151c0f42a2267327f67a3c736
dmatijani <dmatijani21@foi.hr>
2025-01-17 20:17:16 +0100

    CreateGroup forma

--- app/client/src/pages/group/CreateGroup.vue
+++ app/client/src/pages/group/CreateGroup.vue
@@ -47,16 +47,46 @@ const submitForm = async () => {
 
 <template>
     <h2>Kreiraj novu grupu</h2>
-    <form @submit.prevent="submitForm">
-        <label for="name">
-            Naziv grupe
-        </label>
-        <input type="text" id="name" v-model="name" placeholder="Naziv grupe" required />
-        <button type="submit">Kreiraj grupu</button>
-    </form>
-    <SuccessMessage v-if="successMessage != null && successMessage != ''">
-        <span>{{ successMessage }}</span>
-        <RouterLink v-if="newGroupId != null" :to="'/group/' + newGroupId">Odi na novokreiranu grupu</RouterLink>
-    </SuccessMessage>
-    <ErrorMessage v-if="errorMessage != null && errorMessage != ''">{{ errorMessage }}</ErrorMessage>
+    <div class="content">
+        <form @submit.prevent="submitForm">
+            <table>
+                <tr>
+                    <td>
+                        <label for="name">
+                            Naziv grupe
+                        </label>
+                    </td>
+                    <td colspan="3">
+                        <input type="text" id="name" v-model="name" placeholder="Naziv grupe" required />
+                    </td>
+                    <td></td>
+                </tr>
+                <tr class="form-space"></tr>
+                <tr>
+                    <td colspan="2"></td>
+                    <td class="form-center">
+                        <button type="submit">Kreiraj grupu</button>
+                    </td>
+                    <td colspan="2"></td>
+                </tr>
+                <tr>
+                    <td colspan="5" class="form-center">
+                        <SuccessMessage v-if="successMessage != null && successMessage != ''">
+                            <span>{{ successMessage }}</span>
+                            <RouterLink v-if="newGroupId != null" :to="'/group/' + newGroupId">Odi na novokreiranu grupu</RouterLink>
+                        </SuccessMessage>
+                        <ErrorMessage v-if="errorMessage != null && errorMessage != ''">{{ errorMessage }}</ErrorMessage>
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </div>
 </template>
+
+<style scoped>
+.content {
+    max-height: 800px;
+    display: flex;
+    align-items: center;
+}
+</style>
