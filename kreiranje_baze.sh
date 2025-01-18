26983bcaf9f06afb5adf27a26be6d59efffd07c3
dmatijani <dmatijani21@foi.hr>
2025-01-18 18:55:09 +0100

    Skripta za uspješno kreiranje baze

--- kreiranje_baze.sh
+++ kreiranje_baze.sh
@@ -2,37 +2,36 @@
 
 # Po potrebi prije izvršavanja skripte pokrenuti: 'chmod +x kreiranje_baze.sh'
 # Skriptu izvršiti sa: './kreiranje_baze.sh'
-# Po potrebi promijeniti podatke ovdje:
-DB_NAME="tud"
-DB_USER="postgres"
-DB_PASSWORD="postgres"
-# Ako se podaci promijene ovdje, potrebno je u datoteci './app/server/config/config.json' također ažurirati podatke!
+# Po potrebi promijeniti naziv baze ovdje:
+DB_NAME="tud" # Ako se podaci promijene ovdje, potrebno je u datoteci './app/server/config/config.json' također ažurirati podatke!
 
-# Ako skripta ne radi, ručno kreirati novu bazu 'tud' u postgres konzoli i zalijepiti sadržaj datoteke './sql/up.sql' (s konfiguriranjem config datoteke)
+# Ako skripta ne radi, ručno kreirati novu bazu 'tud' u postgres konzoli i zalijepiti sadržaj datoteke './sql/up.sql' (s konfiguriranjem ranije spomenute config datoteke)
 
 SQL_SCRIPT="./sql/up.sql" # Ovo NE mijenjati!
-
-if [ ! -f "$SQL_SCRIPT" ]; then
-    echo "Greška: Datoteka '$SQL_SCRIPT' ne postoji!"
-    exit 1
-fi
+TEMP_SQL_SCRIPT="/tmp/up_tud.sql"
 
 echo "Kreiram bazu '$DB_NAME' i izvršavam '$SQL_SCRIPT'..."
 
-export PGPASSWORD="$DB_PASSWORD"
-
-psql -U "$DB_USER" -c "CREATE DATABASE $DB_NAME;"
-
-if [ $? -ne 0 ]; then
-    echo "Greška pri kreiranju baze."
+if [ ! -r "$SQL_SCRIPT" ]; then
+    echo "Greška: Datoteka '$SQL_SCRIPT' nije čitljiva ili ne postoji."
     exit 1
 fi
 
-psql -U "$DB_USER" -d "$DB_NAME" -f "$SQL_SCRIPT"
-
+cp "$SQL_SCRIPT" "$TEMP_SQL_SCRIPT"
 if [ $? -ne 0 ]; then
-    echo "Greška pri izvršavanju SQL skripte."
+    echo "Greška: Ne mogu kopirati '$SQL_SCRIPT' u '/tmp'."
     exit 1
 fi
 
-echo "Baza '$DB_NAME' je uspešno kreirana!"
+sudo -u postgres psql <<EOF
+CREATE DATABASE $DB_NAME;
+\c $DB_NAME
+\i '$TEMP_SQL_SCRIPT'
+EOF
+
+if [ $? -eq 0 ]; then
+    echo "Baza '$DB_NAME' uspešno kreirana i skripta izvršena!"
+else
+    echo "Greška pri kreiranju baze ili izvršavanju skripte."
+    exit 1
+fi
